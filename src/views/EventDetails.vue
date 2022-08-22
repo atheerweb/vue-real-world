201929fc9959d6dac7a18205175604d4657ab8ef
Yousouf Mustafa Muhammed <76212286+atheerweb@users.noreply.github.com>
2022-08-22 23:23:55 +0200

    vue real world

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import EventService from "@/services/EventService.js";
+export default {
+  props: {
+    id: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      event: null,
+      error: null,
+    };
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then((response) => {
+        this.event = response.data;
+      })
+      .catch((error) => {
+        this.error = error;
+      });
+  },
+};
+</script>
